6a838154cdd61737640c817122932d7786e95390
Joshua Hughes <joshua@plutonus.dev>
2024-12-21 04:16:09 +0100

    style: update WindowHeader component for improved layout and styling

--- renderer/components/meta/WindowHeader.tsx
+++ renderer/components/meta/WindowHeader.tsx
@@ -4,29 +4,29 @@ import { VscChromeMinimize, VscChromeMaximize, VscChromeClose } from 'react-icon
 
 export default () => {
   return (
-    <div className="h-8 titlebar flex justify-between items-center bg-zinc-800 text-zinc-200 font-semibold pr-4 border-b border-zinc-600">
-      <div className="flex flex-row">
-        <div className="relative items-center space-x-4 w-12 h-6">
+    <div className="h-7 titlebar flex justify-between items-center bg-zinc-900 text-zinc-200 font-semibold border-b border-zinc-600">
+      <div className="flex flex-row items-center">
+        <div className="relative items-center space-x-1 px-4 h-4">
           <Image src="/img/vatacars-logo-sm-dark.png" alt="vatACARS Logo" layout="fill" objectFit="contain" />
         </div>
-        <span>Hub v1.0.2</span>
+        <span className="text-sm">Hub {require("../../../package.json").version}</span>
       </div>
-      <div className="flex space-x-2">
+      <div className="flex h-full">
         <a
           onClick={() => window.ipc.send('windowControl', 'minimize')}
-          className="py-1 px-4 titlebar-button hover:bg-zinc-700 transition-all rounded-md"
+          className="px-4 flex items-center h-full titlebar-button hover:bg-zinc-700 transition-all"
         >
           <VscChromeMinimize />
         </a>
         <a
           onClick={() => window.ipc.send('windowControl', 'maximize')}
-          className="py-1 px-4 titlebar-button hover:bg-zinc-700 transition-all rounded-md"
+          className="px-4 flex items-center titlebar-button hover:bg-zinc-700 transition-all"
         >
           <VscChromeMaximize />
         </a>
         <a
           onClick={() => window.ipc.send('windowControl', 'close')}
-          className="py-1 px-4 titlebar-button hover:bg-red-600 transition-all rounded-md"
+          className="px-4 flex items-center titlebar-button hover:bg-red-600 transition-all"
         >
           <VscChromeClose />
         </a>
