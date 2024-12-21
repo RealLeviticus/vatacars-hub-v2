c37971950eb76e4ed9493689c1aa8cadbfb7d65c
Joshua Hughes <joshua@plutonus.dev>
2024-12-21 02:13:59 +0100

    feat: codemod img support

--- renderer/pages/welcome.tsx
+++ renderer/pages/welcome.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import PuffLoader from 'react-spinners/PuffLoader';
 
 export default function HomePage() {
