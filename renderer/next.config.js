5d2bf783c155e5a19e573557fc43e1dfddfb7928
RealLeviticus <levishaw@outlook.com>
2025-04-26 11:59:38 +1000

    Updated Nextron packages to newer versions to allow easier forking and editing

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'export', // Required for static export
+  distDir: '../app', // 👈 This tells Next.js to build into Nextron's "app" folder
   trailingSlash: true,
   images: {
     unoptimized: true,
